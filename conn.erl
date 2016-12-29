6e4b625e7bf3e240c69ab559fe39a611bc441a4a
peter <huangered@hotmail.com>
2016-12-29 10:29:22 -0500

    add peername information

--- conn.erl
+++ conn.erl
@@ -2,12 +2,14 @@
 -export([create/1, accept/2, close/1]).
 
 create(Port) ->
-  {ok, LSock} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false}]),
+  {ok, LSock} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false},{reuseaddr, true}]),
   {ok, Sock} = gen_tcp:accept(LSock),
   Sock.
 
 accept(Sock, Len) ->
-  case gen_tcp:recv(Sock, Len) of
+  {ok, {Address, Port}} = inet:peername(Sock),  
+  io:format("socket: ~p ~p~n", [Address, Port]),
+  case gen_tcp:recv(Sock, 0) of
     {ok, Bin} -> io:format("received ~p~n", [Bin]),
                  {ok, Bin};
     {error, closed} -> {ok, "error"}
