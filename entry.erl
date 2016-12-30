00f550d2027b91e420837352a538abd072b4843b
peter <huangered@hotmail.com>
2016-12-30 04:48:46 -0500

    add gen_server to entry.erl

--- entry.erl
+++ entry.erl
@@ -1,7 +1,56 @@
--module(entry).
--export([server/1]).
+%% -*- erlang-indent-level: 4;indent-tabs-mode: nil -*-
+%% ex: ts=4 sw=4 et
+%% @author huangered <huangered@hotmail.com>
+%% @copyright 2016 huangered, Inc.
 
+-module(entry).
+-behaviour(gen_server).
+
+%% api
+-export([start_link/1, start/0]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {}).
+
+%% api
+start_link(Port) ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [Port], []).		 
+
+start() ->
+  gen_server:cast(?MODULE, start).
+
+%% callback
+
+init([Port]) ->
+  {ok, #state{}}.
+
+handle_call(_Req, _From, State) ->
+    {reply, ignored, State}.
+
+handle_cast(start, State) ->
+    server(5678),
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%% private api
 server(Port) ->
+  io:format("Start..."),
   {ok, Listen} = gen_tcp:listen(Port, [binary, {packet, 0}, {active, false},{reuseaddr, true}]),
   accept(Listen).
 
@@ -24,6 +73,3 @@ loop(Sock) ->
       io:format("Close from ~p:~p~n", [Address, Port]),
       close(Sock)
   end.
-
-
-                                                          
