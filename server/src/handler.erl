c9f75f03978e97c2bcec8409ca4985e90151b9e5
huang yi <huangered@hotmail.com>
2017-01-07 23:12:08 +0800

    add todo

--- server/src/handler.erl
+++ server/src/handler.erl
@@ -7,20 +7,21 @@
 %% - 6 -  op  -  data
 handle(Sock) ->
     case gen_tcp:recv(Sock, 0) of
-    	{ok, Bin} -> 
+        {ok, Bin} -> 
             Line = binary_to_list(Bin),
+            %% match regex d+ w+:w*
             {Len, _ } = string:to_integer(string:substr(Line, 1, 6)),
             OpData = string:substr(Line, 7),
             Index = string:str(OpData, ":"),
             Op = string:substr(OpData, 1, Index - 1),
-    		Data = string:substr(OpData, Index + 1),
+            Data = string:substr(OpData, Index + 1),
             TrimData = string:substr(Data, 1, string:len(Data) - 2 ),
-    		Pack = #package{len = Len, op = list_to_atom(Op), data = TrimData},
+            Pack = #package{len = Len, op = list_to_atom(Op), data = TrimData},
             io:format("Package ~p~n", [Pack]),
-    		core:handle({Sock, Pack}),
+            core:handle({Sock, Pack}),
             handle(Sock);
-    	{error, closed} -> 
-      		io:format("Close from ~n", []),
-      		gen_tcp:close(Sock),
-      		{error, close}
-	end.
+        {error, closed} -> 
+            io:format("Close from ~n", []),
+            gen_tcp:close(Sock),
+            {error, close}
+    end.
