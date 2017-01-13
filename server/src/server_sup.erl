4138dd365b6579f44e39dce6c26fce275f6d96f9
peter <huangered@hotmail.com>
2017-01-13 10:01:33 -0500

    add talk_user_sup to root

--- server/src/server_sup.erl
+++ server/src/server_sup.erl
@@ -10,5 +10,6 @@ start_link() ->
 init([]) ->
 	TalkServer = {core,  {core, start_link, []},  permanent, brutal_kill, worker, [core]},
 	TcpServerSup = {tcpserver_sup, {tcpserver_sup, start_link, []}, permanent, brutal_kill, supervisor, [tcpserver_sup]},
-	Procs = [TalkServer, TcpServerSup],
+	TalkUserSup = {talk_user_sup,{talk_user_sup, start_link,[]},permanent, brutal_kill, supervisor,[talk_user_sup]},
+	Procs = [TalkServer, TcpServerSup, TalkUserSup],
 	{ok, {{one_for_one, 1, 5}, Procs}}.
